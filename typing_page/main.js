f95367ff223a4999656395683db145000f7d04b9
ziheng-hub <78532429+ziheng-hub@users.noreply.github.com>
2021-07-06 00:00:05 +0900

    Update main.js

--- typing_page/main.js
+++ typing_page/main.js
@@ -164,6 +164,6 @@ dataSet.return_Element.addEventListener("click", () => {
   dataSet.container_Element.appendChild(quit_Element);
   dataSet.quit_Element = quit_Element;
   dataSet.quit_Element.addEventListener("click", () => {
-    location.href = "https://app.netlify.com/sites/ziheng-site/overview";
+    location.href = "https://ziheng-site.netlify.app/";
   });
 });
