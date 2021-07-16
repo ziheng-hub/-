a6b90251df8eb718d095d9a669b7a78d6cc25592
ziheng-hub <78532429+ziheng-hub@users.noreply.github.com>
2021-07-16 23:50:07 +0900

    選択式英単語確認問題
    選択肢が出て、英単語を選べる.

---
+++ question_page/data.js
@@ -0,0 +1,30 @@
+//外部からJSONファイルを取得
+function fetch_json() {
+  return (
+    //JSONファイルをJSONオブジェクト化
+    fetch(
+      "https://raw.githubusercontent.com/ziheng-hub/data_json/main/data.json"
+    )
+      .then((response) => {
+        return response.json();
+      })
+      //ランダムでJSONファイルのオブジェクトを取得
+      .then((data) => {
+        random_index = Math.floor(Math.random() * data.length);
+        return data[random_index];
+      })
+  );
+}
+
+//選択肢問題の配列
+let questions = [];
+
+async function data_render() {
+  //JSONデータを取得
+  const json_data = await fetch_json();
+  questions.push(json_data);
+}
+
+for (let index = 0; index < 10; index++) {
+  data_render();
+}
